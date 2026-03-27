1b6c89db2219a68ede0709761b438825daaaef51
BMan-21 <bodhibhatia@gmail.com>
2026-03-26 17:36:27 -0700

    more bomb work

--- scenes/dive_bomber.gd
+++ scenes/dive_bomber.gd
@@ -6,4 +6,5 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("bomb drop"):
 		print("drop")
 		$AnimationPlayer.play("drop bomb")
+		#still figuring out how to drop
 		$Sprite2D.transform.y = delta * bomb_speed
