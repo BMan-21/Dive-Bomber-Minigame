90862d9e15c08be0d610a042391a27c186335984
BMan-21 <bodhibhatia@gmail.com>
2026-03-26 17:34:09 -0700

    wrking on bomb drop

---
+++ scenes/dive_bomber.gd
@@ -0,0 +1,9 @@
+extends AnimatedSprite2D
+
+@export var bomb_speed: int = 50
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("bomb drop"):
+		print("drop")
+		$AnimationPlayer.play("drop bomb")
+		$Sprite2D.transform.y = delta * bomb_speed
