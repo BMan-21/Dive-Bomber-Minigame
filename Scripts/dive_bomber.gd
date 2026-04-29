54a154fe209df72c3c60ee86c374916a24238c2c
BMan-21 <bodhibhatia@gmail.com>
2026-04-28 20:39:46 -0700

    coding

--- Scripts/dive_bomber.gd
+++ Scripts/dive_bomber.gd
@@ -1,16 +1,16 @@
 extends CharacterBody2D
 
-@export var speed:int = 20000
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed:int = 15000
+@onready var altitude_arrow: Label = $"altitude arrow"
 
 
 func _physics_process(delta: float) -> void:
 	velocity.y = delta * speed
-		
+	
+	altitude_arrow.text = str(round(global_position.y * -100)/10)
 	if Input.is_action_pressed("bomb drop"):
 		print("drop")
-		#play drop anim
+		if global_position.y < 350 and global_position.y > 250:
+			pass
 
 	move_and_slide()
