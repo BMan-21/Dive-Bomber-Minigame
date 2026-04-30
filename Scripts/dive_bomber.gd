2b287c5e8b25ccdc6ec3fb0f9bd28a8e7fd0b905
BMan-21 <bodhibhatia@gmail.com>
2026-04-30 11:50:24 -0700

    Update scene transition conditions in dive_bomber.gd

--- Scripts/dive_bomber.gd
+++ Scripts/dive_bomber.gd
@@ -6,16 +6,19 @@ extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
 	velocity.y = delta * speed
-	if global_position.y > -250:
-		pass
+	if global_position.y > -200:
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
 		#play die anim
 	altitude_arrow.text = str(round(global_position.y * -100)/10)
 	if Input.is_action_pressed("bomb drop"):
 		print("drop")
+		
 		if global_position.y > -350 and global_position.y < -250:
 			get_tree().change_scene_to_file("res://scenes/bomb successful.tscn")
 		elif global_position.y < -350:
 			get_tree().change_scene_to_file("res://scenes/bomb missed.tscn")
+		if global_position.y > -250:
+			get_tree().change_scene_to_file("res://scenes/menu.tscn")
 			#play miss anim
 
 	move_and_slide()
